714400292228c3fc4e8f7810c17ea9b65934d98d
Jake Yang <coolinker@gmail.com>
2016-01-24 23:57:09 +0800

    none

---
+++ setup.sh
@@ -0,0 +1,21 @@
+apt-get update
+apt-get install nodejs
+apt-get install npm
+apt-get install git
+
+git clone  https://github.com/teem2/dreem2.git
+git clone https://github.com/coolinker/tako.git
+
+cd dreem2/compositions/
+ln -s -d root/tako tako
+
+cd root/tako
+npm install get-pixels
+npm install http-proxy
+npm install ndarray
+npm install node-rsa
+npm install path
+npm install request
+npm install save-pixels
+npm install seajs
+npm install winston
